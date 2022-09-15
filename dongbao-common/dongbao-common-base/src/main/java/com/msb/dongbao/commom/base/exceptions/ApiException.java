7bdc0791e7a22af504514b1a10ce087c009f532c
Adam <7450214+adam_smith_yep@user.noreply.gitee.com>
2022-09-15 20:49:54 +0800

    add

---
+++ dongbao-common/dongbao-common-base/src/main/java/com/msb/dongbao/commom/base/exceptions/ApiException.java
@@ -0,0 +1,8 @@
+package com.msb.dongbao.commom.base.exceptions;
+
+public class ApiException extends Exception {
+
+    public ApiException(String message) {
+        super(message);
+    }
+}
--- notelist/01 note.md
+++ notelist/01 note.md
@@ -1,2 +1,5 @@
 ### Tools
 beyond compare 文件对比工具
+
+极速工具  
+https://tool.jisuapi.com/urlencode.html
